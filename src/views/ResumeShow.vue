4061611608f716c49a981a9f4345b143090f427a
Kaitlin <klewismurphy@gmail.com>
2021-04-19 21:30:30 -0500

    adds show action with axios req for experiences

---
+++ src/views/ResumeShow.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="resumeShow">
+    <h1>{{ message }}</h1>
+    <h2>Experience</h2>
+    <p> {{ experience.job_title }} </p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Resume",
+      experience: {},
+    };
+  },
+  created: function () {
+    axios.get("/api/experiences/1").then((response) => {
+      console.log(response.data);
+      this.experience = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
